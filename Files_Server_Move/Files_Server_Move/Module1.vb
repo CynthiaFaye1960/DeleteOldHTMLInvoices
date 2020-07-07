3405bc89147763da540bcdf43ec347c675a9262d
Cynthia Ricks <cricks@brookshirebros.com>
2020-07-07 13:53:40 -0500

    Initial commit

---
+++ Files_Server_Move/Files_Server_Move/Module1.vb
@@ -0,0 +1,13 @@
+﻿Imports System.IO
+Module Module1
+
+    Sub Main()
+        Dim filesToMove = From f In New DirectoryInfo("c:\Code\WebApps\ProductLoss_2018\ProductLoss2018\UploadedFiles\").EnumerateFiles("*.txt")
+        Dim FilePathTo As String = "\\bbidaxapp1\Input"
+
+        For Each f In filesToMove
+            f.MoveTo(FilePathTo)
+        Next
+    End Sub
+
+End Module
